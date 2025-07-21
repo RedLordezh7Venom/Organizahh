dffae5fb51a7c40f9be7802300408f5cc747f211
lordpotato <54540149+RedLordezh7Venom@users.noreply.github.com>
2025-07-22 04:21:27 +0530

    just some minor deletion in offline inference

--- scripts/offline_inference.py
+++ scripts/offline_inference.py
@@ -4,12 +4,8 @@ mistral = Llama.from_pretrained(repo_id="Qwen/Qwen2.5-3B-Instruct-GGUF",
                                 filename="*q4_0.gguf",
                                 verbose=False
                                ) 
-
-resp = mistral("Q: Write a short paragraph introducing Elon Musk. A: ",
-           max_tokens=256,
-           stop=["Q:", "\n"])
            
-resp = mistral("Give folder names for these files  :  result.xslx, marks.pdf, icecream.png, student.sqlite",
+resp = mistral("Generate 3 file names",
            max_tokens=256,
            stop=["Q:", "\n"])
 
