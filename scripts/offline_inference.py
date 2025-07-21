2cf1de8e71c31157ddb12da1da0d81a6834c8679
lordpotato <54540149+RedLordezh7Venom@users.noreply.github.com>
2025-07-22 04:22:58 +0530

    made offline inference into a script

--- scripts/offline_inference.py
+++ scripts/offline_inference.py
@@ -1,12 +1,13 @@
 from llama_cpp import Llama
 
-mistral = Llama.from_pretrained(repo_id="Qwen/Qwen2.5-3B-Instruct-GGUF",
+qwen = Llama.from_pretrained(repo_id="Qwen/Qwen2.5-3B-Instruct-GGUF",
                                 filename="*q4_0.gguf",
                                 verbose=False
                                ) 
            
-resp = mistral("Generate 3 file names",
-           max_tokens=256,
-           stop=["Q:", "\n"])
+if __name__ == "__main__":
+    resp = qwen("Generate 3 file names",
+            max_tokens=2048,
+            stop=["Q:", "\n"])
 
-print(resp)
+    print(resp)
